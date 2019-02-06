5d42cabb0247724006e8e93410527a13490fdf22
newforce <jppwellman@gmail.com>
2019-02-06 11:27:42 -0500

    formulated search.js

---
+++ src/scripts/search.js
@@ -0,0 +1,34 @@
+
+// Parks SECTION
+document.querySelector("#parkSearchBtn").addEventListener("click", () => {
+    // Get User's input
+    const searchTerm = document.querySelector("#parkSearchBar").value;
+    // PLACEHOLDER: Your Function (to log it to the console); Example:
+    // searchPark(searchTerm);
+// // PLACEHOLDERS are fx to get all the SECTION information as json
+// "const searchSECTION = SECTIONNameParam => {..."
+})
+
+// Restaurants SECTION
+document.querySelector("#restaurantSearchBtn").addEventListener("click", () => {
+    // Get User's input
+    const searchTerm = document.querySelector("#restaurantSearchBar").value;
+    // PLACEHOLDER: Your Function (to log it to the console); Example:
+    // searchRestaurant(searchTerm);
+})
+
+// Meet-Up(s) SECTION
+document.querySelector("#meetupSearchBtn").addEventListener("click", () => {
+    // Get User's input
+    const searchTerm = document.querySelector("#meetupSearchBar").value;
+    // PLACEHOLDER: Your Function (to log it to the console); Example:
+    // searchMeetup(searchTerm);
+})
+
+// Concert SECTION
+document.querySelector("#concertSearchBtn").addEventListener("click", () => {
+    // Get User's input
+    const searchTerm = document.querySelector("#concertSearchBar").value;
+    // PLACEHOLDER: Your Function (to log it to the console); Example:
+    // searchConcert(searchTerm);
+})

e8b318de140019c5028006f75abe4b50f59b4031
Charles Belcher <belchercharles@yahoo.com>
2019-02-07 10:41:50 -0500

    checking out to master

--- src/scripts/meetupAPIManager.js
+++ src/scripts/meetupAPIManager.js
@@ -8,21 +8,22 @@ document.querySelector("#meetupSearchBtn").addEventListener("click", () => {
     })
         .then(events => events.json())
         .then(parsedEvents => {
-            // console.log(parsedEvents)
+            console.log(parsedEvents)
             let eventsHTMLHeader = `<h1>Here's your suggested ${meetupParam} events...</h1>`
             document.querySelector("#searchResults").innerHTML += eventsHTMLHeader
             // let eventsHTMLString = ""
             for (i = 0; i < 4; i++) {
-                document.querySelector("#searchResults").innerHTML += `<div id="returnedEventDiv-${[i]}"><h3 id="event-${[i]}">${parsedEvents.events[i].name.text}</h3><h4>Scheduled Event Date & Time - ${parsedEvents.events[i].start.local}</h4><p><a href=${parsedEvents.events[i].url} target="_blank">Event Website</a></p><button id="eventBtn-${[i]}">Save to Itinerary</button></div>`
+                document.querySelector("#searchResults").innerHTML += `<div id="returnedEventDiv-${[i]}"><h3 id="event-${[i]}">${parsedEvents.events[i].name.text}</h3><p><a href=${parsedEvents.events[i].url} target="_blank">${parsedEvents.events[i].url}</a></p><button id="eventBtn-${[i]}">Save to Itinerary</button></div>`
+                // <h4>Scheduled Event Date & Time - ${parsedEvents.events[i].start.local}</h4>
                 // console.table(parsedEvents.events[i].name)
             }
             // document.querySelector("#searchResults").innerHTML += eventsHTMLString
         })
 })
 
-
 document.querySelector("#searchResults").addEventListener("click", () => {
     const resultDiv = event.target.id.split("-");
+    // document.querySelector.
         console.log(resultDiv)
         // wholeCard.remove()
     })
